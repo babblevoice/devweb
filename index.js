05f82de4fbc935b230e48232d96096266c3b5279
barcek <barcek@gmx.com>
2021-11-04 17:18:07 +0000

    docs: added comments & modified existing, revised layout, modified naming & added declaration (index.js)

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/* Requirements */
+
 const http = require( "http" )
 const https = require( "https" )
 const path = require( "path" )
 const fs = require( "fs" ).promises
 
 const config = require( "config" )
+
+/* Initialization */
 
 const host = "localhost"
 const port = 8000
@@ -28,12 +32,15 @@ const port = 8000
   }
 */
 
+/* assign settings from config file */
 const localwebroot = config.get( "devweb.localwebroot" )
 const weblocation = config.get( "devweb.proxyhost" )
 const accesstoken = config.get( "devweb.accesstoken" )
 const addressredirects = config.get( "devweb.addressredirects" )
 const mimemap = config.get( "devweb.mimemap" )
 const servicefilepath = config.get ( "devweb.servicefilepath" )
+
+/* Modification */
 
 /*
   Services can be made available from the service file path in config:
@@ -46,6 +53,7 @@ const servicefilepath = config.get ( "devweb.servicefilepath" )
 
 let services = { available: {} };
 
+/* check for services, require if present and parse any command-line arguments */
 fs.access( servicefilepath )
   .then( res => {
     console.log( `Including services in file ${servicefilepath}` )
@@ -87,6 +95,7 @@ function handleArgs() {
   const filename = path.basename( __filename )
   const args = process.argv.slice( process.argv.indexOf( filename ) )
 
+  /* list available flags, each w/ action and optional long form, short form and summary of intent  */
   const flags = [
     {
       long: "help",
@@ -101,20 +110,65 @@ function handleArgs() {
   ]
 
   args.forEach( async ownArg => {
-    // check whether flag and apply
+    /* check whether flag and if so apply */
     flags.forEach( ownFlag => {
       if( ownArg === "-" + ownFlag.short || ownArg === "--" + ownFlag.long ) ownFlag.action()
     } )
-    // check whether service and call
+    /* check whether service and if so call */
     const parts = getURLParts( ownArg )
     if( parts.route[ 0 ] === "/" && parts.route.slice( 1 ) in services.available ) {
-      await handleService( parts.route.slice( 1 ), parts )
+      await invokeService( parts.route.slice( 1 ), parts )
     }
   } )
 }
 
+/* Utility functions */
+
+/* return object containing URL parts */
+function getURLParts( url ) {
+
+  const hasQueryStr = url.includes( "?" )
+
+  const parts = {
+    route: hasQueryStr ? url.slice( 0, url.indexOf( "?" ) ) : url,
+    query: hasQueryStr ? url.slice( url.indexOf( "?" ) ) : "",
+    pairs: {}
+  }
+
+  if( hasQueryStr ) {
+    const keyValPairs = parts.query.slice( 1 ).split( "&" )
+    keyValPairs.forEach( pair => {
+      const keyOrVal = pair.split( "=" )
+      parts.pairs[ keyOrVal[ 0 ] ] = keyOrVal[ 1 ]
+    } )
+  }
+  return parts
+}
+
+/* return URL with path part(s) replaced or not per addressredirects setting */
+function redirectaddress( addr ) {
+  for (key in addressredirects) {
+    if( 0 == addr.indexOf( key ) ) {
+      addr = addr.replace( key, addressredirects[ key ] );
+      console.log( `Redirecting to ${ addr }` )
+    }
+  }
+  return addr
+}
+
+/* Service handling */
+
+/* return result from service call */
+async function invokeService( service, parts, req = {}, res = {} ) {
+  console.log( `Calling service ${ service }` )
+  return await services.available[ service ]( config, parts, req.body )
+}
+
+/* Request handling */
+
 function proxrequest( req, res, data ) {
 
+  /* default options object for GET method */
   const options = {
     host: weblocation,
     path: req.url,
@@ -124,12 +178,13 @@ function proxrequest( req, res, data ) {
     }
   }
 
+  /* add headers for POST, PUT and DELETE */
   if( "GET" != req.method ) {
     options.headers[ "Content-Type" ] = req.headers[ "content-type" ]
     options.headers[ "Content-Length" ] = req.headers[ "content-length" ]
   }
 
-  const httpsreq = https.request(options, (resp) => {
+  const httpsreq = https.request( options, (resp) => {
 
     console.log( "Received response for request", req.method, req.url )
     console.log( "- statusCode:", resp.statusCode)
@@ -163,44 +218,11 @@ function proxrequest( req, res, data ) {
   httpsreq.end()
 }
 
-function redirectaddress( addr ) {
-  for (key in addressredirects) {
-    if( 0 == addr.indexOf( key ) ) {
-      addr = addr.replace( key, addressredirects[ key ] );
-      console.log( `Redirecting to ${ addr }` )
-    }
-  }
-  return addr
-}
-
-function getURLParts( url ) {
-
-  const hasQueryStr = url.includes( "?" )
-
-  const parts = {
-    route: hasQueryStr ? url.slice( 0, url.indexOf( "?" ) ) : url,
-    query: hasQueryStr ? url.slice( url.indexOf( "?" ) ) : "",
-    pairs: {}
-  }
-
-  if( hasQueryStr ) {
-    const keyValPairs = parts.query.slice( 1 ).split( "&" )
-    keyValPairs.forEach( pair => {
-      const keyOrVal = pair.split( "=" )
-      parts.pairs[ keyOrVal[ 0 ] ] = keyOrVal[ 1 ]
-    } )
-  }
-
-  return parts
-}
-
-async function handleService( service, parts, req = {}, res = {} ) {
-  console.log( `Calling service ${ service }` )
-  return await services.available[ service ]( config, parts, req.body )
-}
-
 const handleFileOrProxy = async function( req, res, filename ) {
 
+  let data
+
+  /* check whether file and if not assume URL and make request */
   try {
 
     data = await fs.readFile( localwebroot + filename, "utf8" )
@@ -228,21 +250,24 @@ const handleFileOrProxy = async function( req, res, filename ) {
   return data
 }
 
+/* Server setup */
+
 const server = http.createServer( async function ( req, res ) {
 
   console.log( "Received request:", req.method, req.url )
 
+  /* handle any path part replacement */
   let url = redirectaddress( req.url )
   req.url = url;
   let data = "";
 
   const parts = getURLParts( url )
 
-  // check whether service and call
+  /* check whether service and if so call */
   if( parts.route.slice( 1 ) in services.available ) {
-    data = await handleService( parts.route.slice( 1 ), parts, req, res )
-  // get file or make proxy request
+    data = await invokeService( parts.route.slice( 1 ), parts, req, res )
   }
+  /* get file or make proxy request */
   else {
     const filename = ( "/" == url ) ? url += "index.html" : parts.route
     data = await handleFileOrProxy( req, res, filename )
@@ -259,7 +284,6 @@ const server = http.createServer( async function ( req, res ) {
 
   res.writeHead( 200 )
   res.end( data )
-
 } )
 
 server.listen( port, host, () => {
