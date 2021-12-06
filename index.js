618d68670d12b555507a93d00892809c4db9b2a9
barcek <barcek@gmx.com>
2021-12-06 17:07:51 +0000

    feature: added set option for config items & made corresponding changes, extended argument handling & modified spacing & punctuation (index.js)

--- index.js
+++ index.js
@@ -31,13 +31,11 @@ const port = 8000
   }
 */
 
-/* assign settings from config file */
-const localwebroot = config.get( "devweb.localwebroot" )
-const weblocation = config.get( "devweb.proxyhost" )
-const accesstoken = config.get( "devweb.accesstoken" )
-const addressredirects = config.get( "devweb.addressredirects" )
-const mimemap = config.get( "devweb.mimemap" )
-const servicefilepath = config.get ( "devweb.servicefilepath" )
+/* get devweb settings, assign servicefilepath and declare remaining */
+const c = { ...config.devweb }
+const { servicefilepath } = c
+
+let localwebroot, proxyhost, accesstoken, addressredirects, mimemap
 
 /* Modification */
 
@@ -52,7 +50,7 @@ const servicefilepath = config.get ( "devweb.servicefilepath" )
 
 let services = { available: {} };
 
-/* check for services, require if present and parse any command-line arguments */
+/* check for services, require if present, parse any command-line arguments and start server */
 fs.access( servicefilepath )
   .then( res => {
     console.log( `Including services in file ${servicefilepath}` )
@@ -70,6 +68,8 @@ fs.access( servicefilepath )
   } )
   .then( () => {
     handleArgs()
+    pullConfig()
+    initServer()
   } )
 
 /*
@@ -105,6 +105,37 @@ function handleArgs() {
         console.log( "Options:\n" + optionsStr )
         process.exit()
       }
+    },
+    {
+      long: "set",
+      short: "s",
+      intent: "set a config item, arrays comma-separated (e.g. -s arr \"1,2\") & nested key-value pairs colon-separated (e.g. -s obj \"k:v\")",
+      params: 2,
+      action: allPars => {
+        let [ key, value ] = allPars
+        const cKeys = Object.keys( c )
+        if( !cKeys.includes( key ) ) return console.log( `No devweb config key found for argument ${ key }` )
+        /* handle config item of primitive type */
+        if( [ "string", "number", "boolean" ].includes( typeof c[ key ] ) ) {
+          console.log( `Setting ${ key } to ${ value }` )
+          if( "number" === typeof c[ key ] ) value = parseInt( value )
+          if( "boolean" === typeof c[ key ] ) value = value.toLowerCase() === "true" ? true : false
+          c[ key ] = value
+        }
+        /* handle config item where array */
+        else if( Array.isArray( c[ key ] ) ) {
+          console.log( `Setting ${ key } to hold ${ value.split( "," ).join( ", " ) }` )
+          const valueArr = value ? value.split( "," ) : []
+          c[ key ] = valueArr
+        }
+        /* handle config item of type object */
+        else if( "object" === typeof c[ key ] ) {
+          const valuePair = value ? value.split( ":" ) : []
+          if( 2 !== valuePair.length ) return console.log( "Command-line argument to set ${ key } unclear - not set" )
+          console.log( `Setting ${ key } to hold key ${ valuePair[ 0 ] } with value ${ valuePair[ 1 ] }` )
+          c[ key ] = { ...c[ key ], ...Object.fromEntries( [ valuePair ] ) }
+        }
+      }
     }
   ]
 
@@ -122,6 +153,7 @@ function handleArgs() {
         const nextInd = argInd + 1
         const numPars = flag.params || 0
         const allPars = numPars && args.slice( nextInd, nextInd + numPars )
+        if( allPars.length < numPars ) return console.log( "Insufficient arguments to option", arg )
         allPars
           ? flag.action( allPars.length === 1 ? allPars[ 0 ] : allPars )
           : flag.action()
@@ -137,6 +169,10 @@ function handleArgs() {
 
     if( !usedArg ) console.log( "No option or service found for argument", arg )
   } )
+}
+
+function pullConfig() {
+  ( { localwebroot, proxyhost: weblocation, accesstoken, addressredirects, mimemap } = c )
 }
 
 /* Utility functions */
@@ -164,9 +200,9 @@ function getURLParts( url ) {
 
 /* return URL with path part(s) replaced or not per addressredirects setting */
 function redirectaddress( addr ) {
-  for (key in addressredirects) {
+  for ( key in addressredirects ) {
     if( 0 == addr.indexOf( key ) ) {
-      addr = addr.replace( key, addressredirects[ key ] );
+      addr = addr.replace( key, addressredirects[ key ] )
       console.log( `Redirecting to ${ addr }` )
     }
   }
@@ -267,42 +303,43 @@ const handleFileOrProxy = async function( req, res, filename ) {
   return data
 }
 
-/* Server setup */
+function initServer() {
 
-const server = http.createServer( async function ( req, res ) {
+  const server = http.createServer( async function ( req, res ) {
 
-  console.log( "Received request:", req.method, req.url )
+    console.log( "Received request:", req.method, req.url )
 
-  /* handle any path part replacement */
-  let url = redirectaddress( req.url )
-  req.url = url;
-  let data = "";
+    /* handle any path part replacement */
+    let url = redirectaddress( req.url )
+    req.url = url;
+    let data = "";
 
-  const parts = getURLParts( url )
+    const parts = getURLParts( url )
 
-  /* check whether service and if so call */
-  if( parts.route.slice( 1 ) in services.available ) {
-    data = await invokeService( parts.route.slice( 1 ), parts, req, res )
-  }
-  /* get file or make proxy request */
-  else {
-    const filename = ( "/" == url ) ? url += "index.html" : parts.route
-    data = await handleFileOrProxy( req, res, filename )
-  }
+    /* check whether service and if so call */
+    if( parts.route.slice( 1 ) in services.available ) {
+      data = await invokeService( parts.route.slice( 1 ), parts, req, res )
+    }
+    /* get file or make proxy request */
+    else {
+      const filename = ( "/" == url ) ? url += "index.html" : parts.route
+      data = await handleFileOrProxy( req, res, filename )
+    }
 
-  if( "proxied" == data ) return
+    if( "proxied" == data ) return
 
-  const filext = /(?:\.([^.]+))?$/.exec( req.url )
-  const mimetype = mimemap[ filext[ 0 ] ] || "text/html"
+    const filext = /(?:\.([^.]+))?$/.exec( req.url )
+    const mimetype = mimemap[ filext[ 0 ] ] || "text/html"
 
-  res.setHeader( "Content-Type", mimetype )
-  res.setHeader( "Cache-Control", "public, max-age=0" );
-  res.setHeader( "Expires", new Date( Date.now() ).toUTCString() )
+    res.setHeader( "Content-Type", mimetype )
+    res.setHeader( "Cache-Control", "public, max-age=0" );
+    res.setHeader( "Expires", new Date( Date.now() ).toUTCString() )
 
-  res.writeHead( 200 )
-  res.end( data )
-} )
+    res.writeHead( 200 )
+    res.end( data )
+  } )
 
-server.listen( port, host, () => {
-  console.log( `Serving from directory ${localwebroot} at http://${host}:${port}` )
-} )
+  server.listen( port, host, () => {
+    console.log( `Serving from directory ${ localwebroot } at http://${ host }:${ port }` )
+  } )
+}
